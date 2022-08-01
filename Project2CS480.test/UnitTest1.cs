a850501cb1f88ebd9d3efe3831864a30ff5934c1
justin Reisz <justin@wesleyreisz.com>
2022-08-01 11:33:23 -0400

    initial commit

---
+++ Project2CS480.test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Project2CS480.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Project2CS480/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+[assembly: InternalsVisibleTo("Project2CS480.test")]
+namespace Project2CS480
+{
+    class Project2CS480
+    {
+        private const Boolean DEBUG = false;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
+
