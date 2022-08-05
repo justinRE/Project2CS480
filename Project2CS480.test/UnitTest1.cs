dab1ae804f580b8cb0c3b082b677957fd68eb260
justin Reisz <justin@wesleyreisz.com>
2022-08-05 15:42:23 -0400

    The project

--- Project2CS480.test/UnitTest1.cs
+++ Project2CS480.test/UnitTest1.cs
@@ -10,18 +10,33 @@ namespace Project2CS480.test
         [Test]
         public void TestSetup()
         {
-            Tea hash = new Tea();
-            string input = "encrypt";
-            Assert.AreEqual(hash.setup(), true);
+            Tea tea = new Tea();
+            tea.setup();
+            string input = "e";
+            Assert.AreEqual(true, tea.isEncrypting);
         }
 
         [Test]
         public void TestEncrypt()
         {
-            Tea hash = new Tea();
-            UInt32[] value = new UInt32[];
-            UInt32[] key = new UInt32[];
+            Tea tea = new Tea();
+            string key = "Hello world";
+            tea.keyToBlocks(key);
+            UInt32[] keyblock = new UInt32[4];
+          //  keyblock = ["H","E"]
+           // Assert.AreEqual(hash.keyToUintBlocks);
         }
+
+        [Test]
+        public void ConvertStringToUInt()
+        {
+            Tea hash = new Tea();
+            UInt32 output;
+            string[] input = new string[4];
+
+            //Assert.AreEqual(ConvertStringToUInt(input), true);
+        }
+        
 
     }
 }
--- Project2CS480/Program.cs
+++ Project2CS480/Program.cs
@@ -8,42 +8,170 @@ namespace Project2CS480
 {
     class Tea
     {
+        public bool isEncrypting = false;
         private const Boolean DEBUG = false;
         static void Main(string[] args)
         {
-            Tea hash = new Tea();
-            hash.setup();
+            
+
+            Tea tea = new Tea();
+            tea.setup();
+            // input is key and data to encrypt/decrypt
+            string key = tea.keyInput();
+            string dataInput = tea.Datainput();
+            string keyBlocks = tea.keyToBlocks(key);
+            UInt32[] keyBlocksArray = tea.keyToUInt32Blocks(keyBlocks);
+            if (tea.isEncrypting)
+            {
+                string final = tea.encryptText(dataInput, keyBlocksArray);
+                Console.WriteLine(final);
+            }
+            else {
+                string final = tea.decryptText(dataInput, keyBlocksArray);
+                Console.WriteLine(final);
+            }
         }
 
 
-        internal bool setup()
+        internal void setup()
         {
             string input = "";
-            bool output = true;
             bool holder = true;
 
             while (holder == true)
             {
-                Console.WriteLine("Would you like to Encrypt or Decrypt something? ");
+                Console.WriteLine("Would you like to Encrypt or Decrypt something? (E/D)");
                 input = Console.ReadLine();
                 input.ToLower();
-                if (input == "encrypt")
+                if (input == "e")
                 {
-                    output = true;
-                    holder = false;
+                    isEncrypting = true;
+                    break;
                 }
-                else if (input == "decrypt")
+                else if (input == "d")
                 {
-                    output = false;
-                    holder = false;
+                    break;
                 }
                 else
-                    Console.WriteLine("Enter encrypt or decrypt");
+                    Console.WriteLine("Enter encrypt or decrypt (E/D)");
+            }
+        }
+
+        internal string keyInput()
+        {
+            string key;
+            Console.WriteLine("Enter the key value: ");
+            key = Console.ReadLine();
+   
+            return key;
+        }
+
+        internal string Datainput()
+        {
+            string inputData;
+
+            Console.WriteLine("Enter the value to be ecrypted or decrypted: ");
+            inputData = Console.ReadLine(); 
+
+            return inputData;
+        }
+
+        internal string keyToBlocks(string key)
+        {
+            if (key.Length == 0)
+                throw new ArgumentException("Key has to be 1-16 characters");
+
+
+            int length = key.Length;
+            if (length < 128)
+            {
+                length = 128 - length;
+                for (int i = 0; i < length; i++)
+                {
+                    key = key + " ";
+                }
+            }
+            return key;
+        }
+
+        internal UInt32[] keyToUInt32Blocks(string key)
+        {
+            UInt32[] keyBlocks = new UInt32[32];
+            int holder = 0;
+            for (int i = 0; i < key.Length; i += 4)
+            {
+                string parsestring = key.Substring(i, 4);
+                keyBlocks[holder++] = convertStringToUInt(parsestring);
             }
 
+            return keyBlocks;
+        }
 
+        internal string encryptText(string inputData, UInt32[] keyBlocks)
+        {
+
+            if (inputData.Length % 2 != 0) inputData += '\0'; // Makes sure array is even so it can be input into the encryption algorithm (uses pairs of data)
+            byte[] dataBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(inputData);
+
+            string cipher = string.Empty;
+            uint[] tempData = new uint[2];
+            for (int i = 0; i < dataBytes.Length; i += 2)
+            {
+                tempData[0] = dataBytes[i];
+                tempData[1] = dataBytes[i + 1];
+                encrypt(tempData, keyBlocks);
+                cipher += convertUIntToString(tempData[0]) +
+                                  convertUIntToString(tempData[1]);
+            }
+
+            return cipher;
+        }
+
+
+        public string decryptText(string inputData, UInt32[] keyBlocks)
+        {
+            int x = 0;
+            uint[] temp = new uint[2];
+            byte[] dataBytes = new byte[inputData.Length / 8 * 2];
+            for (int i = 0; i < inputData.Length; i += 8)
+            {
+                temp[0] = convertStringToUInt(inputData.Substring(i, 4));
+                temp[1] = convertStringToUInt(inputData.Substring(i + 4, 4));
+                decrypt(temp, keyBlocks);
+                dataBytes[x++] = (byte)temp[0];
+                dataBytes[x++] = (byte)temp[1];
+            }
+
+            string decipheredString =
+                    System.Text.ASCIIEncoding.ASCII.GetString(dataBytes,
+                                                              0, dataBytes.Length);
+
+            // gets rid of the padding in the final result if it has it
+            if (decipheredString[decipheredString.Length - 1] == '\0')
+                decipheredString = decipheredString.Substring(0, decipheredString.Length - 1);
+            return decipheredString;
+        }
+
+        internal uint convertStringToUInt(string Input)
+        //the opposite of how ConvertUIntToString works, without the & so it doesn't return only one byte
+        {
+            uint output;
+            output = ((uint)Input[0]);
+            output += ((uint)Input[1] << 8);
+            output += ((uint)Input[2] << 16);
+            output += ((uint)Input[3] << 24);
             return output;
+        }
 
+        internal string convertUIntToString(uint Input)
+         // Shifts the bits in the input to make the string UInt, & 0xFF makes it return only 1 byte(a character)- 4 bytes is 32 bits which is what a UInt32 holds and what we need for the TEA Algorithm 
+        {
+            System.Text.StringBuilder output = new System.Text.StringBuilder();
+            output.Append((char)((Input & 0xFF)));
+            output.Append((char)((Input >> 8) & 0xFF));
+            output.Append((char)((Input >> 16) & 0xFF));
+            output.Append((char)((Input >> 24) & 0xFF));
+            return output.ToString();
         }
 
         void encrypt(UInt32[] value, UInt32[] key)
